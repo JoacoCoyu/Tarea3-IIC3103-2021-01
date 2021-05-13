d2e2c3d35eb134b1cc9e0b3f3d82aa5a2de16bbe
JoacoCoyu <jacouyoumdjian@uc.cl>
2021-05-13 14:03:13 -0400

    trying to display map

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import logo from './logo.svg';
 import './style/App.css';
-import Map from './components/map' ;
+import MapInterface from './components/map';
+//import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 const App = () => {
   return (
-    <div className="App">
-      <Map />
-    </div>
+      // <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      //   <TileLayer
+      //     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      //     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      //   />
+      //   <Marker position={[51.505, -0.09]}>
+      //     <Popup>
+      //       A pretty CSS3 popup. <br /> Easily customizable.
+      //     </Popup>
+      //   </Marker>
+      // </MapContainer>
+      <div>
+        <MapInterface />
+      </div>
   );
 }
 
