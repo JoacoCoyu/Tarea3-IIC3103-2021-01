02d0fb4f2c1a67856627f852a30436c5c8be8a6e
JoacoCoyu <jacouyoumdjian@uc.cl>
2021-05-13 12:00:17 -0400

    first commit hello world

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './style/App.css';
+import Map from './components/map' ;
+
+const App = () => {
+  return (
+    <div className="App">
+      <Map />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/map.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Map = (props) => {
+
+    return (
+        <div>
+            <p>Hello, this is a map</p>
+        </div>
+    )
+
+}
+
+export default Map;
